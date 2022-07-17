1696964ab456e6f0c81d1baf6067a57cc2716834
RazumeetsyaDanila <danil.kozhemyakin.99@mail.ru>
2022-07-18 00:13:01 +0300

    some changes...

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -11,14 +11,14 @@ const MainPage: React.FC = () => {
 
     return (
         <div className={s.container}>
-            <img src={avatar} className={s.img}></img>
+            <img src={avatar} className={s.img} alt=""></img>
             <p className={s.sayMyName}>Danil Kozhemyakin</p>
             <p className={s.info}>Full Stack Software Developer</p>
 
             <div className={s.contacts}>                
-                <a href="https://t.me/razumeetsya_danila" target="_blank"><img className={s.telegramImg} src={telegramImg} alt="" /></a>
-                <a href="mailto:danil.kozhemyakin.99@mail.ru" target="_blank"><img className={s.mailImg} src={mailImg} alt="" /></a>
-                <a href="https://github.com/RazumeetsyaDanila" target="_blank"><img className={s.gitImg} src={githubImg} alt="" /></a>
+                <a href="https://t.me/razumeetsya_danila" target="_blank" rel="noreferrer"><img className={s.telegramImg} src={telegramImg} alt="" /></a>
+                <a href="mailto:danil.kozhemyakin.99@mail.ru" target="_blank" rel="noreferrer"><img className={s.mailImg} src={mailImg} alt="" /></a>
+                <a href="https://github.com/RazumeetsyaDanila" target="_blank" rel="noreferrer"><img className={s.gitImg} src={githubImg} alt="" /></a>
             </div>
 
             <NavLink to='/files/rezume.pdf' target="_blank" download className={s.downloadBtn}>Download resume</NavLink>
