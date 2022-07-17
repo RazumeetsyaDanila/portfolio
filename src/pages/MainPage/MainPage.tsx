f33bd98dfe7863de5ee399ad5ff054bab2d57487
RazumeetsyaDanila <danil.kozhemyakin.99@mail.ru>
2022-07-17 23:55:13 +0300

    some changes...

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import s from './mainPage.module.scss'
 import { NavLink } from 'react-router-dom';
 import avatar from '../../img/avatar.jpg'
+import githubImg from '../../img/github.png'
+import telegramImg from '../../img/telegram.png'
+import mailImg from '../../img/mail.png'
 
 
 const MainPage: React.FC = () => {
@@ -11,6 +14,13 @@ const MainPage: React.FC = () => {
             <img src={avatar} className={s.img}></img>
             <p className={s.sayMyName}>Danil Kozhemyakin</p>
             <p className={s.info}>Full Stack Software Developer</p>
+
+            <div className={s.contacts}>                
+                <a href="https://t.me/razumeetsya_danila" target="_blank"><img className={s.telegramImg} src={telegramImg} alt="" /></a>
+                <a href="mailto:danil.kozhemyakin.99@mail.ru" target="_blank"><img className={s.mailImg} src={mailImg} alt="" /></a>
+                <a href="https://github.com/RazumeetsyaDanila" target="_blank"><img className={s.gitImg} src={githubImg} alt="" /></a>
+            </div>
+
             <NavLink to='/files/rezume.pdf' target="_blank" download className={s.downloadBtn}>Download resume</NavLink>
         </div>
     );
